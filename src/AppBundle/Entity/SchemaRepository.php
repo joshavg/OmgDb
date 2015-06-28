4d03f0f56bab8d1642e8d67d0b2d4c642c07855f
Josha von Gizycki <mail@joshavg.de>
2015-06-28 15:59:33 +0200

    added creator to schemas

--- src/AppBundle/Entity/SchemaRepository.php
+++ src/AppBundle/Entity/SchemaRepository.php
@@ -2,27 +2,49 @@
 namespace AppBundle\Entity;
 
 use laniger\Neo4jBundle\Architecture\Neo4jRepository;
+use laniger\Neo4jBundle\Architecture\Neo4jClientConsumer;
+use laniger\Neo4jBundle\Architecture\Neo4jClientWrapper;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class SchemaRepository
 {
-    use Neo4jRepository;
+    use Neo4jRepository {
+        Neo4jRepository::__construct as neo;
+    }
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function __construct(Neo4jClientWrapper $client, TokenStorage $storage)
+    {
+        $this->neo($client);
+        $this->user = $storage->getToken()->getUser();
+    }
 
     public function newSchema(array $formdata)
     {
         $this->client->cypher('
-            CREATE (n:Schema)
+             MATCH (u:User)
+             WHERE u.name = {username}
+            CREATE (n:Schema)<-[r:Created]-(u)
                SET n.name = {name}
         ', [
-            'name' => $formdata['name']
+            'name' => $formdata['name'],
+            'username' => $this->user->getUsername()
         ]);
     }
 
     public function fetchForOverview()
     {
         return static::transformToArray($this->client->cypher('
-             MATCH (n:Schema)
+             MATCH (n:Schema)<-[r:Created]-(u:User)
+             WHERE u.name = {username}
             RETURN n
             ORDER BY n.name
-        '));
+        ', [
+            'username' => $this->user->getUsername()
+        ]));
     }
 }
