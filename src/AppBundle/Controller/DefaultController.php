40efbc67e7959de0eb0f73a474b40015bf603548
Josha von Gizycki <mail@joshavg.de>
2016-05-18 22:15:58 +0200

    removed schema creation form from default page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,9 +5,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Architecture\ContainerServices;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\Type\SchemaType;
-use AppBundle\Entity\Schema;
 
 class DefaultController extends Controller
 {
@@ -17,17 +14,9 @@ class DefaultController extends Controller
      * @Route("/", name="index")
      * @Template
      */
-    public function indexAction(Request $req)
+    public function indexAction()
     {
-        $form = $this->createForm(SchemaType::class, new Schema(), [
-            'action' => $this->generateUrl('schema_insert')
-        ]);
-
-        $form->handleRequest($req);
-        $form->isValid();
-
         return [
-            'newSchemaForm' => $form->createView()
         ];
     }
 }
