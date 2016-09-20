3a940f7fb4a3ca639cc7806d4d4da59e58ffd173
Josha von Gizycki <mail@joshavg.de>
2016-09-20 11:58:18 +0200

    refactored attribute data types to contain their respective field types themselves added date data type - not yet functional

--- src/AppBundle/Entity/AttributeDataType.php
+++ src/AppBundle/Entity/AttributeDataType.php
@@ -1,8 +1,15 @@
 <?php
 namespace AppBundle\Entity;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class AttributeDataType
 {
+    const UNKOWN = 'unknown';
 
     const TEXT = 'text';
 
@@ -14,36 +21,53 @@ class AttributeDataType
 
     const BOOLEAN = 'boolean';
 
+    const DATE = 'date';
+
     /**
      * @return AttributeDataType[]
      */
     public static function getTypes()
     {
         return [
-            new AttributeDataType(static::TEXT),
-            new AttributeDataType(static::MARKDOWN),
-            new AttributeDataType(static::LABEL),
-            new AttributeDataType(static::NUMBER),
-            new AttributeDataType(static::BOOLEAN)
+            new AttributeDataType(static::TEXT, TextareaType::class),
+            new AttributeDataType(static::MARKDOWN, TextareaType::class),
+            new AttributeDataType(static::LABEL, TextType::class),
+            new AttributeDataType(static::NUMBER, NumberType::class),
+            new AttributeDataType(static::BOOLEAN, CheckboxType::class),
+            new AttributeDataType(static::DATE, DateType::class)
         ];
     }
 
     public static function getByName($name)
     {
-        // TODO make the instances somewhat singleton-ish
-        return new AttributeDataType($name);
+        $all = static::getTypes();
+        foreach($all as $type) {
+            if($type->getName() === $name) {
+                return $type;
+            }
+        }
+
+        return new AttributeDataType(static::UNKOWN, TextType::class);
     }
 
     private $name;
 
-    private function __construct($name)
+    private $fieldType;
+
+    private function __construct($name, $fieldType)
     {
         $this->name = $name;
+        $this->fieldType = $fieldType;
     }
 
     public function getName()
     {
         return $this->name;
+    }
+
+    public function getFieldType()
+    {
+        return $this->fieldType;
     }
 
     public function __toString()
