dbf7180e6e123eb7d38cfbb44a0de51067f722d9
Josha von Gizycki <mail@joshavg.de>
2017-08-11 09:20:15 +0200

    row selection by selecting rows

--- web/js/omgdb.js
+++ web/js/omgdb.js
@@ -1,7 +1,16 @@
 document.getElementById('select-all').addEventListener('click', function (e) {
     const rowBoxes = document.getElementsByClassName('use-checkbox');
 
-    for(const box of rowBoxes) {
+    for (const box of rowBoxes) {
         box.checked = e.target.checked;
     }
 });
+
+const useTrs = document.getElementsByClassName('use-tr');
+for (const tr of useTrs) {
+    tr.addEventListener('click', function (e) {
+        for (const box of tr.getElementsByClassName('use-checkbox')) {
+            box.checked = !box.checked;
+        }
+    });
+}
